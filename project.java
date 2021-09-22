ec59f535ceb584c918b0ed8a1112bc97dd50f2bd
lazysandwich <90320205+lazysandwich@users.noreply.github.com>
2021-09-22 14:11:50 +0300

    Update project.java

--- project.java
+++ project.java
@@ -1,1 +1,15 @@
+package com.company;
 
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("my game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setUndecorated(true);
+        frame.setVisible(true);
+        frame.add(new Main());
+    }
+}
